b5f6bcdb9801b49e17eaf9f863e5be5fd0c5c4a9
cristiano <cristiano.nog.woody@gmail.com>
2023-09-11 19:26:08 -0300

    feat: creating CreateUserUseCaseDTO

---
+++ src/api/use-cases/create-user/CreateUserUseCaseDTO.ts
@@ -0,0 +1,11 @@
+export class CreateUserUseCaseDTO {
+  public name: string
+  public email: string
+  public password: string
+
+  constructor({ name, email, password }: CreateUserUseCaseDTO) {
+    this.name = name
+    this.email = email
+    this.password = password
+  }
+}
