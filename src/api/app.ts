85d273b24c7a0a4be905ba6db3a015e444058a02
cristiano <cristiano.nog.woody@gmail.com>
2023-09-11 19:24:09 -0300

    refactor: using user Routes in app

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import userRouter from './http/routes/userRoutes'
 
 const app = express()
 
@@ -7,5 +8,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
+app.use(userRouter)
 
 export default app
