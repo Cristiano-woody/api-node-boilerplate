beb1a09f679d9cfc8c27ccf986d0bf2e890b8d5a
cristiano <cristiano.nog.woody@gmail.com>
2023-09-11 19:25:17 -0300

    style: env index

--- src/api/env/index.ts
+++ src/api/env/index.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config'
-import {z} from 'zod'
+import { z } from 'zod'
 
 const envSchema = z.object({
-  PORT_API: z.coerce.number().default(3333)
+  PORT_API: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
+if (_env.success === false) {
   console.error('🛑 Invalid environment variable', _env.error.format())
   throw new Error('Invalid environment variable')
 }
