9fd072488e9378d3baffabc1d6f76f7a5e91db0e
cristiano <cristiano.nog.woody@gmail.com>
2023-09-11 19:29:42 -0300

    feat: creating makeCreateUserController

---
+++ src/api/http/controllers/create-user/makeCreateUserController.ts
@@ -0,0 +1,10 @@
+import { makeCreateUserUseCase } from '../../../use-cases/create-user/makeCreateUserUseCase'
+import CreateUserController from './CreateUserController'
+
+export const makeCreateUserController = (): CreateUserController => {
+  const createUserUseCase = makeCreateUserUseCase()
+  const createUserUseCaseController = new CreateUserController(
+    createUserUseCase,
+  )
+  return createUserUseCaseController
+}
