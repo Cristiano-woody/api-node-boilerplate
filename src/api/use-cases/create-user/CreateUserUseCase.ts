befdedaa2c4bbd5b93acc3361684f0109b3ec067
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-14 16:30:22 -0300

    refactor: creating create useCase implementation

--- src/api/use-cases/create-user/CreateUserUseCase.ts
+++ src/api/use-cases/create-user/CreateUserUseCase.ts
@@ -1,14 +1,15 @@
+import { ICreateUserRepository } from '@/api/repositories/create-user/ICreateUserRepository'
 import { CreateUserUseCaseDTO } from './CreateUserUseCaseDTO'
 import { ICreateUserUseCase } from './ICreateUserUseCase'
+import User from '@/api/models/User'
 
 class CreateUserUseCase implements ICreateUserUseCase {
+  constructor(private readonly createUserRepository: ICreateUserRepository){}
+  
   async execute(data: CreateUserUseCaseDTO): Promise<CreateUserUseCaseDTO> {
-    const user = new CreateUserUseCaseDTO({
-      name: data.name,
-      email: data.email,
-      password: data.password,
-    })
-    return user
+    const newUser = new User({name: data.name, email: data.email, passwordHash: data.password })
+    console.log(await this.createUserRepository.CreateUser(newUser))
+    return data
   }
 }
 
