45683ba4ab8e2c2f02c03b87e393fee9fc087934
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-14 16:31:37 -0300

    refactor: injetando rCreateUserRepository in CreateUserUseCase

--- src/api/use-cases/create-user/makeCreateUserUseCase.ts
+++ src/api/use-cases/create-user/makeCreateUserUseCase.ts
@@ -1,6 +1,8 @@
+import PgCreateUserRepository from '@/api/repositories/create-user/PgCreateUserRepository'
 import CreateUserUseCase from './CreateUserUseCase'
 
 export const makeCreateUserUseCase = (): CreateUserUseCase => {
-  const createUserUseCase = new CreateUserUseCase()
+  const createUserRepository = new PgCreateUserRepository()
+  const createUserUseCase = new CreateUserUseCase(createUserRepository)
   return createUserUseCase
 }
