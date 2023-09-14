f253fe42affe159b9813ed5df1235913117ec290
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-14 16:28:22 -0300

    refactor: creating query method in PgRepository

--- src/api/repositories/PgRepository.ts
+++ src/api/repositories/PgRepository.ts
@@ -2,14 +2,26 @@ import { Client } from "pg"
 import { env } from "../env"
 
 abstract class PgRepository {
-  connect(): Client {
-    return new Client({
+  private _client: Client
+  constructor() {
+    this._client = new Client({
       host: "localhost",
       port: 5432,
       database: "postgres",
       user: 'postgres',
       password: "postgres",
     })
+  }
+
+  async query(sql: string): Promise<any> {
+    this._client.connect()
+    const result = await this._client?.query(sql)
+    this._client.end()
+    return result
+  }
+
+  async comand(): Promise<void> {
+
   }
 }
 
