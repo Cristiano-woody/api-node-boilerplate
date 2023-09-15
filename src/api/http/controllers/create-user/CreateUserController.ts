0b845fd64f480e05d54692684e0554d4912ca82a
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-15 16:26:59 -0300

    style: createUserController

--- src/api/http/controllers/create-user/CreateUserController.ts
+++ src/api/http/controllers/create-user/CreateUserController.ts
@@ -1,25 +1,27 @@
-import { ICreateUserUseCase } from '../../../use-cases/create-user/ICreateUserUseCase'
-import { ICreateUserController } from './ICreateUserController'
+import { type ICreateUserUseCase } from '../../../use-cases/create-user/ICreateUserUseCase'
+import { type ICreateUserController } from './ICreateUserController'
 import type { Request, Response } from 'express'
 import { z } from 'zod'
 
 class CreateUserController implements ICreateUserController {
-  constructor(private readonly CreateUserUseCase: ICreateUserUseCase) {}
-  public async handle(req: Request, res: Response): Promise<Response> {
+  constructor (private readonly CreateUserUseCase: ICreateUserUseCase) {}
+
+  public async handle (req: Request, res: Response): Promise<Response> {
     const requestSchema = z.object({
       name: z.string(),
       email: z.string().email(),
-      password: z.string().min(6),
+      password: z.string().min(6)
     })
     const ValidRequest = requestSchema.safeParse(req.body)
-    if (ValidRequest.success === false) {
+    if (!ValidRequest.success) {
       return res.status(400).json(ValidRequest.error)
     }
+
     try {
       const user = await this.CreateUserUseCase.execute({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: req.body.password
       })
       return res.status(200).send(user)
     } catch (error) {
