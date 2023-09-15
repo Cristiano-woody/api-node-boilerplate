6aa8cf61784c330462cd32080c04f68dda851400
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-15 16:27:15 -0300

    style: ICreateUserController

--- src/api/http/controllers/create-user/ICreateUserController.ts
+++ src/api/http/controllers/create-user/ICreateUserController.ts
@@ -1,4 +1,4 @@
-import { Response, Request } from 'express'
+import { type Response, type Request } from 'express'
 
 export interface ICreateUserController {
   handle: (req: Request, res: Response) => Promise<Response>
