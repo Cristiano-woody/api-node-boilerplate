3d6fe0514aa22c357f728831e9652f4ab05d12e1
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-15 16:27:54 -0300

    style: UserRoutes

--- src/api/http/routes/userRoutes.ts
+++ src/api/http/routes/userRoutes.ts
@@ -1,10 +1,8 @@
-import { Request, Response, Router } from 'express'
+import { type Request, type Response, Router } from 'express'
 import { makeCreateUserController } from '../controllers/create-user/makeCreateUserController'
 
 const userRouter = Router()
 
-userRouter.post('/user', (req: Request, res: Response) => {
-  makeCreateUserController().handle(req, res)
-})
+userRouter.post('/user', (req: Request, res: Response) => { void makeCreateUserController().handle(req, res) })
 
 export default userRouter
