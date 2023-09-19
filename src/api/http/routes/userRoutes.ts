8f4030f0b030c2c42501d573d6d015047aeb21b7
cristiano <cristiano.nog.woody@gmail.com>
2023-09-18 22:42:50 -0300

    refactor: adding get all users route in UsersRoutes

--- src/api/http/routes/userRoutes.ts
+++ src/api/http/routes/userRoutes.ts
@@ -1,8 +1,11 @@
 import { type Request, type Response, Router } from 'express'
 import { makeCreateUserController } from '../controllers/create-user/makeCreateUserController'
+import { makeGetAllUsersController } from '../controllers/get-all-users/makeGetAllUsersController'
 
 const userRouter = Router()
 
 userRouter.post('/user', (req: Request, res: Response) => { void makeCreateUserController().handle(req, res) })
+
+userRouter.get('/user', (req: Request, res: Response) => { void makeGetAllUsersController().handle(req, res) })
 
 export default userRouter
