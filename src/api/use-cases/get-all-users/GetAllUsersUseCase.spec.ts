af0d9001e545a53ee121ae011e70bcfd24a137f1
cristiano <cristiano.nog.woody@gmail.com>
2023-09-18 22:49:46 -0300

    style: modified imports in getAllUsers Use Case tester

--- src/api/use-cases/get-all-users/GetAllUsersUseCase.spec.ts
+++ src/api/use-cases/get-all-users/GetAllUsersUseCase.spec.ts
@@ -1,14 +1,14 @@
 import { describe, it, expect, beforeEach } from 'vitest'
-import InMemoryGetUserRepository from '../../repositories/get-user/InMemoryGetUserRepository'
 import GetAllUsersUseCase from './GetAllUsersUseCase'
-import User from '../../models/User'
+import User from '../../entities/User'
+import InMemoryUserRepository from '../../repositories/UserRepository/InMemoryUserRepository'
 
-let getUserRepository: InMemoryGetUserRepository
+let userRepository: InMemoryUserRepository
 let getAllUsersUseCase: GetAllUsersUseCase
 describe('tests of get all users use case', () => {
   beforeEach(() => {
-    getUserRepository = new InMemoryGetUserRepository()
-    getAllUsersUseCase = new GetAllUsersUseCase(getUserRepository)
+    userRepository = new InMemoryUserRepository()
+    getAllUsersUseCase = new GetAllUsersUseCase(userRepository)
   })
   it('shold be able to get all users', async () => {
     await expect(getAllUsersUseCase.execute()).resolves.not.toThrow()
@@ -18,7 +18,7 @@ describe('tests of get all users use case', () => {
     expect(Array.isArray(users)).toEqual(true)
   })
   it('should be able to check if this return of method he has user properties', async () => {
-    await getUserRepository.createUser(new User({ name: 'cris', email: 'cis@example.com', passwordHash: '123123' }))
+    await userRepository.createUser(new User({ name: 'cris', email: 'cis@example.com', passwordHash: '123123' }))
     const users = await getAllUsersUseCase.execute()
     expect(users[0]).toHaveProperty('name')
     expect(users[0]).toHaveProperty('id')
