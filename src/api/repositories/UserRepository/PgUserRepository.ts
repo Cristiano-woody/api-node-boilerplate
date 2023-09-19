e5f8f32dfa448c29c26bf5d2fbc0510c50fa15d7
cristiano <cristiano.nog.woody@gmail.com>
2023-09-18 22:44:10 -0300

    feat: create PgUserRepository

---
+++ src/api/repositories/UserRepository/PgUserRepository.ts
@@ -0,0 +1,24 @@
+import type User from '../../entities/User'
+import PgRepository from '../PgRepository'
+import { type IUserRepository } from './IUserRepository'
+
+class PgUserRepository extends PgRepository implements IUserRepository {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor () {
+    super()
+  }
+
+  async createUser (user: User): Promise<User> {
+    const query = `INSERT INTO "users" (id, name, email, password_hash) VALUES ('${user.id}', '${user.name}', '${user.email}', '${user.passwordHash}');`
+    await super.query(query)
+    return user
+  }
+
+  async getAllUsers (): Promise<User[]> {
+    const query = 'SELECT * FROM "users"'
+    const result = await super.query(query)
+    return result
+  }
+}
+
+export default PgUserRepository
