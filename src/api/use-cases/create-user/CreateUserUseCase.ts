84e549b61eb41b326a8ed5f74831ce2b71e2ae1f
cristiano <cristiano.nog.woody@gmail.com>
2023-09-18 22:47:22 -0300

    style: create user use case

--- src/api/use-cases/create-user/CreateUserUseCase.ts
+++ src/api/use-cases/create-user/CreateUserUseCase.ts
@@ -1,11 +1,11 @@
-import { type ICreateUserRepository } from '../../repositories/create-user/ICreateUserRepository'
 import { type CreateUserUseCaseDTO } from './CreateUserUseCaseDTO'
 import { type ICreateUserUseCase } from './ICreateUserUseCase'
 import User from '../../entities/User'
 import { type ICrypto } from '../../helpers/crypto/ICrypto'
+import { type IUserRepository } from '../../repositories/UserRepository/IUserRepository'
 
 class CreateUserUseCase implements ICreateUserUseCase {
-  constructor (private readonly createUserRepository: ICreateUserRepository, private readonly crypto: ICrypto) {}
+  constructor (private readonly createUserRepository: IUserRepository, private readonly crypto: ICrypto) {}
 
   async execute (data: CreateUserUseCaseDTO): Promise<Omit<User, 'passwordHash'>> {
     const newUser = new User({ name: data.name, email: data.email, passwordHash: await this.crypto.hash(data.password) })
