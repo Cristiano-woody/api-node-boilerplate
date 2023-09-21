ee5afd142a7c68d1576d73ba8eca66273d31fec9
cristiano <cristiano.nog.woody@gmail.com>
2023-09-21 09:35:09 -0300

    refactor: modifying property name in IcheateUserUseCase

--- src/api/use-cases/create-user/ICreateUserUseCase.ts
+++ src/api/use-cases/create-user/ICreateUserUseCase.ts
@@ -2,5 +2,5 @@ import type User from '../../entities/User'
 import { type CreateUserUseCaseDTO } from './CreateUserUseCaseDTO'
 
 export interface ICreateUserUseCase {
-  execute: (data: CreateUserUseCaseDTO) => Promise<Omit<User, 'passwordHash'>>
+  execute: (data: CreateUserUseCaseDTO) => Promise<Omit<User, 'password_hash'>>
 }
