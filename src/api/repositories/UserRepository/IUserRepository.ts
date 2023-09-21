1ae7558a77211daf554961f97465582d879af322
cristiano <cristiano.nog.woody@gmail.com>
2023-09-21 09:32:14 -0300

    refactor: adding new method in IUserRepository

--- src/api/repositories/UserRepository/IUserRepository.ts
+++ src/api/repositories/UserRepository/IUserRepository.ts
@@ -2,5 +2,6 @@ import type User from '../../entities/User'
 
 export interface IUserRepository {
   createUser: (user: User) => Promise<User>
-  getAllUsers: () => Promise<User[]>
+  getAllUsers: () => Promise<Omit<User[], 'password_hash'>>
+  getUserByID: (userId: string) => Promise<User | null>
 }
