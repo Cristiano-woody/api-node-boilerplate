1e1986c95dfe0e3898d0e17fd9ba32d614874025
cristiano <cristiano.nog.woody@gmail.com>
2023-09-21 09:30:34 -0300

    refactor: modified propriety name of User entity

--- src/api/entities/User.ts
+++ src/api/entities/User.ts
@@ -3,17 +3,17 @@ import { randomUUID } from 'crypto'
 class User {
   public name: string
   public email: string
-  public passwordHash: string
+  public password_hash: string
   public id?: string
   constructor (user: {
     name: string
     email: string
-    passwordHash: string
+    password_hash: string
     id?: string
   }) {
     this.name = user.name
     this.email = user.email
-    this.passwordHash = user.passwordHash
+    this.password_hash = user.password_hash
     this.id = user.id
     if (user.id === null || user.id === undefined) {
       this.id = randomUUID()
