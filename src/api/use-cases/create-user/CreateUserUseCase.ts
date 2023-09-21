1518712119dba5deebed5f7ce3ae47fffb2b1fe1
cristiano <cristiano.nog.woody@gmail.com>
2023-09-21 09:34:53 -0300

    refactor: modifying property name in cheateUserUseCase

--- src/api/use-cases/create-user/CreateUserUseCase.ts
+++ src/api/use-cases/create-user/CreateUserUseCase.ts
@@ -7,10 +7,10 @@ import { type IUserRepository } from '../../repositories/UserRepository/IUserRep
 class CreateUserUseCase implements ICreateUserUseCase {
   constructor (private readonly createUserRepository: IUserRepository, private readonly crypto: ICrypto) {}
 
-  async execute (data: CreateUserUseCaseDTO): Promise<Omit<User, 'passwordHash'>> {
-    const newUser = new User({ name: data.name, email: data.email, passwordHash: await this.crypto.hash(data.password) })
+  async execute (data: CreateUserUseCaseDTO): Promise<Omit<User, 'password_hash'>> {
+    const newUser = new User({ name: data.name, email: data.email, password_hash: await this.crypto.hash(data.password) })
     const user = await this.createUserRepository.createUser(newUser)
-    const userWithoutPassword: Omit<User, 'passwordHash'> = {
+    const userWithoutPassword: Omit<User, 'password_hash'> = {
       name: user.name,
       email: user.email,
       id: user.id
