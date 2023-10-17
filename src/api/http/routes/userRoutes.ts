fa9d8295f9a9c9c44740adb4b737e7c09a724521
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-10-17 16:20:14 -0300

    feat: criando rota de deletar usuario

--- src/api/http/routes/userRoutes.ts
+++ src/api/http/routes/userRoutes.ts
@@ -1,11 +1,14 @@
 import { type Request, type Response, Router } from 'express'
 import { makeCreateUserController } from '../controllers/create-user/makeCreateUserController'
 import { makeGetAllUsersController } from '../controllers/get-all-users/makeGetAllUsersController'
+import { makeDeleteUserController } from '../controllers/delete-user/makeDeleteUserController'
 
 const userRouter = Router()
 
 userRouter.post('/user', (req: Request, res: Response) => { void makeCreateUserController().handle(req, res) })
 
 userRouter.get('/user', (req: Request, res: Response) => { void makeGetAllUsersController().handle(req, res) })
+
+userRouter.delete('/user/:id', (req: Request, res: Response) => { void makeDeleteUserController().handle(req, res) })
 
 export default userRouter
