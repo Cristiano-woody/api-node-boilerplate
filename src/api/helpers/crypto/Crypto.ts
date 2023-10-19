c1cc32df811735adb68beb01ceaef405fc9d2e47
cristiano <cristiano.nog.woody@gmail.com>
2023-10-18 22:46:04 -0300

    feat: adicionando metodo compare na classe crypto

--- src/api/helpers/crypto/Crypto.ts
+++ src/api/helpers/crypto/Crypto.ts
@@ -1,9 +1,13 @@
 import { type ICrypto } from './ICrypto'
-import { hash } from 'bcryptjs'
+import { hash, compare } from 'bcryptjs'
 
 class Crypto implements ICrypto {
   async hash (data: string): Promise<string> {
     return await hash(data, 6)
+  }
+
+  async compare (data: string, hash: string): Promise<boolean> {
+    return await compare(data, hash)
   }
 }
 
