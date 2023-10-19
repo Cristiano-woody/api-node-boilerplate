8d2cb7fa3511c09b35028d3663c5abf2fa8860c2
cristiano <cristiano.nog.woody@gmail.com>
2023-10-18 22:51:45 -0300

    feat: usando middleware de autenticacao nas rotas

--- src/api/http/routes/userRoutes.ts
+++ src/api/http/routes/userRoutes.ts
@@ -4,17 +4,21 @@ import { makeGetAllUsersController } from '../controllers/get-all-users/makeGetA
 import { makeDeleteUserController } from '../controllers/delete-user-by-id/makeDeleteUserController'
 import { makeGetUserByIdController } from '../controllers/get-user-by-id/makeGetUserByIdController'
 import { makeUpdateUserController } from '../controllers/update-user/makeUpdateUserController'
+import { auth } from '../../middlewares/auth'
+import { makeUserLoginController } from '../controllers/user-login/makeUserLoginController'
 
 const userRouter = Router()
 
 userRouter.post('/user', (req: Request, res: Response) => { void makeCreateUserController().handle(req, res) })
 
-userRouter.get('/user', (req: Request, res: Response) => { void makeGetAllUsersController().handle(req, res) })
+userRouter.get('/user', auth, (req: Request, res: Response) => { void makeGetAllUsersController().handle(req, res) })
 
-userRouter.get('/user/:id', (req: Request, res: Response) => { void makeGetUserByIdController().handle(req, res) })
+userRouter.get('/user/:id', auth, (req: Request, res: Response) => { void makeGetUserByIdController().handle(req, res) })
 
-userRouter.delete('/user/:id', (req: Request, res: Response) => { void makeDeleteUserController().handle(req, res) })
+userRouter.delete('/user/:id', auth, (req: Request, res: Response) => { void makeDeleteUserController().handle(req, res) })
 
-userRouter.put('/user/:id', (req: Request, res: Response) => { void makeUpdateUserController().handle(req, res) })
+userRouter.put('/user/:id', auth, (req: Request, res: Response) => { void makeUpdateUserController().handle(req, res) })
+
+userRouter.post('/user/auth', (req: Request, res: Response) => { void makeUserLoginController().handle(req, res) })
 
 export default userRouter
